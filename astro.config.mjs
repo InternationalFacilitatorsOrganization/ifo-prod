baec56ea726c00f9fc2a581e22f6fed313edbd1a
Scott Finkel <scott.finkel@gmail.com>
2025-12-21 17:06:30 -0800

    Add build adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import clerk from "@clerk/astro";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), clerk()],
+  output: 'server',
+  integrations: [react(), clerk(), netlify()],
   vite: {
     plugins: [tailwindcss()],
   },
