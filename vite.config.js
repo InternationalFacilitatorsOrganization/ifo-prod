48d56f025b1c862f030bd0bdc8da9644f92f0236
Scott Finkel <scott.finkel@gmail.com>
2025-12-21 16:54:33 -0800

    Add netlify vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import netlify from "@netlify/vite-plugin";
 
 export default defineConfig({
     server: {
         allowedHosts: [
             'devserver-add-profile-page--ifo-prod.netlify.app'
         ]
-    }
+    },
+    plugins: [netlify()],
 })
