3924a8ae8a7d78ff79972f645d26074019ab3482
Scott Finkel <scott.finkel@gmail.com>
2026-02-07 11:28:05 -0800

    Add March training page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const { heroui } = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/@heroui/theme/dist/components/(table|checkbox|form|spacer).js",
   ],
   theme: {
